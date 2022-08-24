f046b54994502482f020c05b46bcae0f879f4b00
Lester Lecong <lecong.lester@gmail.com>
2022-08-24 22:47:30 +0800

    Adding comment for WindowsApplicationHandler class.

--- ApplicationHandler/Windows/WindowsApplicationHandler.h
+++ ApplicationHandler/Windows/WindowsApplicationHandler.h
@@ -6,7 +6,23 @@ class WindowsApplicationHandler : public ApplicationHandlerImpl
 {
 public:
     WindowsApplicationHandler();
+
+    /**
+     * @brief Checking the application if it is running or not
+     *
+     * @param app - Application object that is listed on the configuration file.
+     * @return true - if the application is currently running or active.
+     * @return false  - if currently closed.
+     */
     virtual bool isProcessRunning(Application app);
+
+    /**
+     * @brief Perform to run the executable file.
+     *
+     * @param app - holds the executable name to run.
+     * @return int - if values are below or equal to 33 it means error except 0 which means success.
+     *               Above 33 will be the HANDLE instance value.
+     */
     virtual int runApp(Application app);
 };
 #endif
